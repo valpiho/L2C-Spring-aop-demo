deb7701a0c3ced08dbbbb90b7866d6fc49cb5f8b
Valentin Piho <val.piho@gmail.com>
2020-10-26 23:08:52 +0200

    Combining Pointcuts

--- src/com/pibox/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/pibox/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,10 +12,22 @@ public class MyDemoLoggingAspect {
     @Pointcut("execution(* com.pibox.aopdemo.dao.*.*(..))")
     public void forDaoPackage() {}
 
+    @Pointcut("execution(* com.pibox.aopdemo.dao.*.get*(..))")
+    public void getter() {}
+
+    @Pointcut("execution(* com.pibox.aopdemo.dao.*.set*(..))")
+    public void setter() {}
+
+    @Pointcut("forDaoPackage() && !getter() || setter()")
+    public void forDaoPackageNoGetterSetter() {}
+
     @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=======>>>> Executing @Before advice on addAccount()");
     }
 
-
+    @Before("forDaoPackageNoGetterSetter()")
+    public void performApiAnalytics() {
+        System.out.println("=======>>>> Performing API analytics");
+    }
 }
