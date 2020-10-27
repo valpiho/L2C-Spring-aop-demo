2cae11f796f639cbb3ade97a916cb2660c65dd6c
Valentin Piho <val.piho@gmail.com>
2020-10-27 22:43:10 +0200

    Adding additional info for Accounts

--- src/com/pibox/aopdemo/dao/AccountDAO.java
+++ src/com/pibox/aopdemo/dao/AccountDAO.java
@@ -3,6 +3,9 @@ package com.pibox.aopdemo.dao;
 import com.pibox.aopdemo.Account;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class AccountDAO {
 
@@ -13,5 +16,20 @@ public class AccountDAO {
     public boolean doWork() {
         System.out.println(getClass() + ": doWork()");
         return false;
+    }
+
+    public List<Account> findAccounts() {
+
+        List<Account> accountList = new ArrayList<>();
+
+        Account account1 = new Account("John", "Silver");
+        Account account2 = new Account("Madhu", "Platinum");
+        Account account3 = new Account("Lucas", "Gold");
+
+        accountList.add(account1);
+        accountList.add(account2);
+        accountList.add(account3);
+
+        return accountList;
     }
 }
